3abd2f21e0a8f9f3b5f722c90a3759b610de5fab
bilalcoder-28 <bilalasif2007@gamil.com>
2026-02-03 04:03:22 +0500

    js incomplete

---
+++ app.js
@@ -0,0 +1,76 @@
+// const  result = [{
+//     name:"Bilal",
+//     rollNumber:213231,
+//     Samistar : {
+//         subject : {
+//             phy: 67,
+//             chemistry:87,
+//             math:92 ,
+//             persentage: {
+//                 Map ()
+//             }
+
+//         }
+
+//     }
+// }]
+
+
+
+
+let studentOne = [{
+    name: "bilal",
+    rollNumber: 231423,
+    samistar: [{
+        samistarOne: {
+            phy: 82,
+            che: 67,
+            math: 78,
+          
+
+        }
+       
+    },{
+        samistarTwo: {
+            phy: 82,
+            che: 67,
+            math: 78,
+          
+
+        }
+    },{
+        samistarThree: {
+            phy: 82,
+            che: 67,
+            math: 78,
+          
+
+        }
+    },{
+        samistarfour: {
+            phy: 82,
+            che: 67,
+            math: 78,
+          
+
+        }
+    },{
+       
+    }]
+        
+
+
+}]
+
+let persentage = studentOne[1]
+
+
+console.log(persentage);
+
+
+
+
+
+
+
+console.log(studentOne)
